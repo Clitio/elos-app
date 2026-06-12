b5e4d4526b06939a071363dfb61b912361f4ccf4
Thiago Barbosa <thiago.dasilvabarbosa@student.griffith.ie>
2026-06-12 02:05:02 +0100

    fix useEffect import in RegisterPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { signInWithPopup, signInWithEmailAndPassword } from 'firebase/auth'
 import { auth, googleProvider } from '../firebase'
